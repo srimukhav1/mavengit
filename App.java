d2f98927acb2da100da73d4ccb9fbb8e329b3ee5
srimukhav1 <srimukhavasireddy@gmail.com>
2017-12-22 10:44:30 -0800

    maven pro

---
+++ App.java
@@ -0,0 +1,13 @@
+package mavengroup1.mavenartifact1.mavengroup1.mavenartifact1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Maven project2" );
+    }
+}
